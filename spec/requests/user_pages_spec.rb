e06b0639fb7c7a1bdbf1c59630611af2acedbe12
ozanyildiz <ozanyyildiz@gmail.com>
2013-07-07 00:15:11 +0300

    Add user_pages tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -51,17 +51,17 @@ describe "UserPages" do
       it "should create user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
 
-    describe "after saving a user" do 
+      describe "after saving a user" do 
 
-      before { click_button submit }
+        before { click_button submit }
 
-      let(:user) { User.find_by_email("user@example.com") }
+        let(:user) { User.find_by_email("user@example.com") }
 
-      #it { should have_selector('title', text: user.name) }
-      #it { should have_selector('div.alert.alert-success'), text: "Welcome" }
-      #it { should have_link('Sign out') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
     end
   end
 end
